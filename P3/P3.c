3f3b94332692ae247947ec2edc55bbc870c2da64
milesfrom1900 <93260150+milesfrom1900@users.noreply.github.com>
2024-01-01 14:32:54 +0100

    P3 solved

--- P3/P3.c
+++ P3/P3.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-
-long long int GetlargestPrime(long long int n){
+long long int GetGreatestPrime(long long int n){
     long long int p=0;
 
-    while (n != p){
-        for (p = 2; p <= n; p++){
-            if (n % p == 0){
-                printf("%lli ",p);
+    for (p = 2; p <= n; p++){
+        while (n % p == 0){
+            printf("%lli ",p);
                 if (n == p)
                     return n;
                 n=n/p;
-            }
         }
-    }
-    return n;
+    } 
 }
 
 int main(){
     long long int n = 600851475143;
-    printf("Result: %lli\n", GetlargestPrime(n));
+    printf("Result: %lli \n", GetGreatestPrime(n));
 }
