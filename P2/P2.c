8a1f6e0e123127aa86717471e6408b3853a34735
Lukas Miles <lukas.miles97@gmail.com>
2025-11-09 21:32:01 +0100

    Optimize P2

--- P2/P2.c
+++ P2/P2.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+//Sum of even numbers in Fibonacci sequence less than 4000000
+
 int main(){
-    int Num=1;
-    int prevNum=1;
-    int tempNum=0;
-    int Sum=0;
-    while (Num < 4000000){
-        if(Num%2==0){Sum+=Num;}
-        tempNum=Num;
-        Num=Num+prevNum;
-        prevNum=tempNum;
+    int Sum=0, prev=2, prev2=0; //Start at 0 since 1 is not even
+    while (Sum < 4000000){
+        Sum += prev;
+        int temp = prev;
+        prev=4*prev+prev2; //=F(n-1)*4+F(n-2)
+        prev2=temp;
     }
     printf("Result: %i\n", Sum);
 }
+
+/* Every third value is even
+2  
+3   Odd
+5   Odd
+8 
+13  Odd
+21  Odd
+34
+55  Odd
+89  Odd 
+144
+
+Deriving function
+2=0*4+0
+8=2*4+0
+34=8*4+2
+144=34*4+8
+*/
