e424a78aba8d64a65b756b065ac1b2dff756e1bb
Lukas Miles <lukas.miles97@gmail.com>
2025-11-13 23:13:43 +0100

    Optimize P6

--- P6/P6.c
+++ P6/P6.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 #include "../common/utils.h"
 
 llu SumOfSquares(llu n){
-    llu sum=0;
+    
+    llu sum=pow((n+1)/2);
+    
+    /*llu sum=0;
     for (int i = 1; i <= n; i++)
     {
         sum= sum + i*i;
     }
+    */
     return sum;
 }
 
@@ -24,3 +29,15 @@ int main(){
     llu nNum = 100;
     printf("Result: %llu \n",SquareOfSum(nNum)-SumOfSquares(nNum));
 }
+
+/*Optimizaton workshop
+    Square of sums
+    For 10: 1+10=11, 2+9=11, ...
+    Sum=(nMin+nMax)/2
+
+    Sum of squares
+    Squares 1   4   9   16  25
+    Sum     1   5   14  30  55
+
+    
+*/
